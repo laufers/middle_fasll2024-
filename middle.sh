985dcbea545c4467a0c7232170a0af1d48a95cca
Mark Laufersweiler <laufers@ou.edu>
2024-09-27 20:09:30 +0000

    added how to use this script

--- middle.sh
+++ middle.sh
@@ -1,5 +1,7 @@
 # script to return the middle text of a file
 # create Sep 27, 2024
-# creator Mark Laufersweiler 
+# creator Mark Laufersweiler \
+# Usage: 
+# include in first position, file path and name via command line 
 
 head -n 9 "$1" | tail -n 3 
